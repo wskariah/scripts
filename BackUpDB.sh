86de88d163c917e81c9d2afd49d138e86bfb61a4
Will Skariah <wskariah@gmail.com>
2020-12-31 13:33:47 -0500

    Update BackUpDB.sh
    updated

--- BackUpDB.sh
+++ BackUpDB.sh
@@ -2,34 +2,36 @@
 
 
 echo "Enter the port number for the host you want to backup the DB on: "
-read FROM 
-echo
+while read FROM
+do
+echo "$FROM" done >/tmp/test
 
 echo "Enter the IP address of the environment that you are doing the DB backup on: "
-read IP
+while read IP
+do
+echo "$IP" done >
+
+echo "Stopping $FROM server"
 echo
 
-echo "Stopping $FROM hub"
-echo
-
-        ssh -i ~/.ssh/yocto.rsa -p $FROM root@$IP exec /etc/init.d/basecamp stop
+        ssh -i ~/.ssh/mytest.rsa -p $FROM root@$IP exec /etc/init.d/mydb stop
 
 echo
 
 sleep 3
 
-echo "Starting to backup the database and eprom files"
+echo "Starting to backup the database"
 
-        ssh -i ~/.ssh/yocto.rsa -p $FROM root@$IP "mkdir /home/DB_Backup"
-        ssh -i ~/.ssh/yocto.rsa -p $FROM root@$IP "cp /var/sqlite/ZWave.db /home/DB_Backup"
-        ssh -i ~/.ssh/yocto.rsa -p $FROM root@$IP exec /sbin/eprog -d /home/DB_Backup/eeprom.hex
+        ssh -i ~/.ssh/mytest.rsa -p $FROM root@$IP "mkdir /home/DB_Backup"
+        ssh -i ~/.ssh/mytest.rsa -p $FROM root@$IP "cp /var/sqlite/mydb.db /home/DB_Backup"
 
 
-echo "Starting $FROM hub"
+echo "Starting $FROM server"
 echo
 
-        ssh -i ~/.ssh/yocto.rsa -p $FROM root@$IP exec /etc/init.d/basecamp start
+        ssh -i ~/.ssh/mytest.rsa -p $FROM root@$IP exec /etc/init.d/myservice start
 
 sleep 3
 
 echo "DB backup on $FROM is complete"
+
